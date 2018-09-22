ca45179c2f8baa011d200af20156a3b5c31bd292
aalk0004 <43328606+aalk0004@users.noreply.github.com>
2018-09-22 12:59:11 +1000

    adding setters and getters

--- LineItem.java
+++ LineItem.java
@@ -11,7 +11,7 @@ public class LineItem
     private String [] items;
     private int quantity;
     private double totalPrice;
-    private int capacity;
+
 
     /**
      * Constructor for objects of class LineItem
@@ -19,9 +19,36 @@ public class LineItem
     public LineItem()
     {
         // initialise instance variables
-        
+        items = new String[0];
+        quantity = 0;
+        totalPrice = 0.00;
     }
-
+    // initialise getters
+    public String[] getItems()
+    {
+        return items; 
+    }
+    
+    public int getQuantity()
+    {
+        return quantity;
+    }
+    
+        public double getTotalPrice()
+    {
+        return totalPrice;
+    }
+    
+    // initialise setters
+        public void setItems(String[] newItems)
+    {
+        items = newItems;
+    }
+    
+        public void setTotalPrice(double newTotalPrice)
+    {
+        totalPrice = newTotalPrice;
+    }
 
     /**
      * An example of a method - replace this comment with your own
