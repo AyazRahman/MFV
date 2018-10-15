7451dfe6220513d4437bda2bc6f0bdd9da77787f
aalk0004 <43328606+aalk0004@users.noreply.github.com>
2018-10-15 21:39:38 +1100

    validatioin class doc comments

--- Validation.java
+++ Validation.java
@@ -31,6 +31,12 @@ public class Validation
         saleTypes = new ArrayList<>(Arrays.asList("kg", "bag", "box", "punnet", "piece", "bundle", "stalk"));                                       
     }
     
+    /**
+     * This method is used to validate entred password to be
+     * more than 5 in length of string.
+     * @param pwd This is the first paramter to validatePwd method.
+     * @return boolean This returns true if password lenght is more than 4 .
+     */
     public boolean validatePwd(String pwd)
     {
         if (pwd.length() >= 5)
@@ -40,10 +46,9 @@ public class Validation
     }
     
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * This method is used to validate entred name.
+     * @param name This is the first paramter to validateName method.
+     * @return boolean This returns true if name length greater then 2 and less that 10.
      */
     public boolean validateName(String name)
     {
@@ -59,6 +64,11 @@ public class Validation
         return true;
     }
     
+    /**
+     * This method is used to validate entred cartd number.
+     * @param cardNumber This is the first paramter to validateCardNumber method.
+     * @return boolean This returns true if entred number length is 16 digits.
+     */
     public boolean validateCardNumber(String cardNumber)
     {
         cardNumber = cardNumber.replaceAll("\\s","");
@@ -77,6 +87,11 @@ public class Validation
         return true;
     }
     
+    /**
+     * This method is used to validate entred email.
+     * @param email This is the first paramter to validateEmail method.
+     * @return boolean This returns true if entred email is valid.
+     */
     public boolean validateEmail(String email)
     {
          String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
@@ -90,6 +105,11 @@ public class Validation
         return pat.matcher(email).matches(); 
     }
     
+    /**
+     * This method is used to validate entred address.
+     * @param address This is the first paramter to validateAddress method.
+     * @return boolean This returns true if entred address is valid.
+     */
     public boolean validateAddress(String address)
     {
         String addressRegex = "\\d+\\s+([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)";
@@ -100,6 +120,11 @@ public class Validation
         return address.matches(addressRegex);
     }
     
+    /**
+     * This method is used to validate entred post code.
+     * @param pc This is the first paramter to validatePostcodeDelivery method.
+     * @return boolean This returns true if entred post code is valid and with in the range of two suburbs.
+     */
     public boolean validatePostcodeDelivery(String pc)
     {
         if (pc.trim() == "")
@@ -112,6 +137,11 @@ public class Validation
         return false;
     }
     
+    /**
+     * This method is used to validate entred post code.
+     * @param pc This is the first paramter to validatePostcodeDelivery method.
+     * @return boolean This returns true if entred post code is met with regex for australian post codes.
+     */
     public boolean validatePostcode(String pc)
     {
         String postcodes = "^(0[289][0-9]{2})|([1345689][0-9]{3})|(2[0-8][0-9]{2})|(290[0-9])|(291[0-4])|(7[0-4][0-9]{2})|(7[8-9][0-9]{2})$";
@@ -124,6 +154,11 @@ public class Validation
         return false;
     }
     
+    /**
+     * This method is used to validate entred state.
+     * @param state This is the first paramter to validateState method.
+     * @return boolean This returns true if entred state is valid.
+     */
     public boolean validateState(String state)
     {
         if (state.trim() == "")
@@ -136,6 +171,11 @@ public class Validation
         return false;
     }
     
+    /**
+     * This method is used to validate entred name on card.
+     * @param name This is the first paramter to validateCardName method.
+     * @return boolean This returns true if entred card name is valid.
+     */
     public boolean validateCardName(String name)
     {
         for (char c : name.toCharArray()) {
@@ -150,6 +190,11 @@ public class Validation
         return true;
     }
     
+    /**
+     * This method is used to validate entred ccv on card.
+     * @param ccv This is the first paramter to validateCCV method.
+     * @return boolean This returns true if entred card name is from 3 to 4 digts.
+     */
     public boolean validateCCV(String ccv)
     {
         String s = ccv.trim();
@@ -160,6 +205,11 @@ public class Validation
         return s.matches(regex);
     }
     
+    /**
+     * This method is used to validate entred product name.
+     * @param prodName This is the first paramter to validateProdName method.
+     * @return boolean This returns true if entred product name is valid.
+     */
     public boolean validateProdName(String prodName)
     {
         for (char c : prodName.toCharArray()) {
@@ -174,6 +224,11 @@ public class Validation
         return true;
     }
     
+    /**
+     * This method is used to validate shelf life.
+     * @param days This is the first paramter to validateShelfLife method.
+     * @return boolean This returns true if entred shelf life is valid.
+     */
     public boolean validateShelfLife(String days)
     {
         if (days.trim().matches("^[0-9]{1,3}$") && days.trim() != "0")
@@ -184,6 +239,11 @@ public class Validation
             return false;
     }
     
+    /**
+     * This method is used to validate sale type.
+     * @param saleType This is the first paramter to validateSaleType method.
+     * @return boolean This returns true if entred sale type is valid.
+     */
     public boolean validateSaleType(String saleType)
     {
         if (saleType.trim() == "")
@@ -196,6 +256,11 @@ public class Validation
         return false;
     }
     
+    /**
+     * This method is used to validate quantity.
+     * @param qty This is the first paramter to validateQty method.
+     * @return boolean This returns true if entred quantity is valid.
+     */
     public boolean validateQty(String qty)
     {
         if (qty.trim().matches("^[0-9]{1,3}$") && qty.trim() != "0")
@@ -206,9 +271,13 @@ public class Validation
             return false;
     }
     
-    //validating date entred for system report
-    //checking the formate to match dd/mm/yyyy
-    //prevent future dates
+    /**
+     * This method is used to validate the date entred for system report and
+     * checking the formate to match dd/mm/yyyy and
+     * prevent future dates.
+     * @param entredDate This is the first paramter to validateDate method.
+     * @return boolean This returns true if entred date is valid.
+     */
     public static boolean validateDate(String entredDate)
     {
         String dateRegex = "^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$";
@@ -247,6 +316,11 @@ public class Validation
         return false;
     }
     
+    /**
+     * This method is used to validate entred word
+     * @param word This is the first paramter to validateWord method.
+     * @return boolean This returns true if entred word is valid.
+     */
     public boolean validateWord(String word)
     {
         for (char c : word.toCharArray()) {
@@ -261,6 +335,13 @@ public class Validation
         return true;
     }
     
+    /**
+     * This method is used to validate the receipt date
+     * checking the formate to match dd/mm/yyyy and
+     * prevent past dates.
+     * @param receiptDate This is the first paramter to validateReceiptDate method.
+     * @return boolean This returns true if entred receipt date is valid.
+     */
     public boolean validateReceiptDate(String receiptDate)
     {
         String dateRegex = "^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$";
@@ -294,6 +375,11 @@ public class Validation
         return false;
     }
     
+    /**
+     * This method is used to validate a string number
+     * @param string This is the first paramter to valNum method.
+     * @return boolean This returns true if entred word is valid.
+     */
     public boolean valNum(String string)
     {
         return string.matches("[0-9]+");
